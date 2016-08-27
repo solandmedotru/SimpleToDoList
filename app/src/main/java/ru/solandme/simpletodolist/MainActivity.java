d1e2a96a3294e91d23f9ce2f59df084089fd28c8
Andrey Solomin <solomin.andrey@gmail.com>
2016-08-27 21:41:21 +0300

    Added share action provider

--- app/src/main/java/ru/solandme/simpletodolist/MainActivity.java
+++ app/src/main/java/ru/solandme/simpletodolist/MainActivity.java
@@ -1,21 +1,27 @@
 package ru.solandme.simpletodolist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ShareActionProvider;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener {
     ArrayList<String> myList = new ArrayList<>();
     ArrayAdapter listAdapter;
     EditText editText;
     ListView todoList;
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +46,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         myList.add(editText.getText().toString());
         listAdapter.notifyDataSetChanged();
+        setShareIntent();
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         myList.remove(i);
         listAdapter.notifyDataSetChanged();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider)shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
+        return true;
+    }
+
+    private void setShareIntent() {
+
+        if (mShareActionProvider != null) {
+            String allTextList = "ToDo List, just do it:";
+            for (String s : myList) {
+                allTextList = allTextList + "\n" + s;
+            }
+
+            // create an Intent with the contents of the TextView
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "ToDo List, just do it:");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, allTextList);
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 }
